9ba133d74844d136e8f028880a8aed403939ae4a
Christoffer Niska <ChristofferNiska@gmail.com>
2013-02-08 00:31:53 +0200

    Add files

---
+++ ModelGroupForm.php
@@ -0,0 +1,144 @@
+<?php
+/**
+ * ModelGroupForm.php class file.
+ * @author Christoffer Niska <christoffer.niska@gmail.com>
+ * @copyright Copyright &copy; Christoffer Niska 2013-
+ * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ */
+
+/**
+ * Form model that can handle other models in a massive way.
+ */
+class ModelGroupForm extends CFormModel 
+{
+	/**
+	 * @var CModel[] $models the model instances (name=>model).
+	 */
+	public $models = array();
+
+	/**
+	 * Does magic for getting attributes within in group models.
+	 * @param string $name the property name or event name
+	 * @return mixed the property value, event handlers attached to the event, or the named behavior
+	 * @throws CException if the property or event is not defined
+	 */
+	public function __get($name) 
+	{
+		$name = $this->resolveModelAttribute($name);
+		if (is_array($name)) 
+		{
+			list($modelName, $attribute) = $name;
+			if (!empty($attribute) && isset($this->models[$modelName]))
+				return $this->models[$modelName]->{$attribute};
+		}
+		return parent::__get($name);
+	}
+
+	/**
+	 * Sets the attribute values in a massive way.
+	 * @param array $values attribute values (name=>value) to be set.
+	 * @param boolean $safeOnly whether the assignments should only be done to the safe attributes.
+	 */
+	public function setAttributes($values, $safeOnly = true) 
+	{
+		parent::setAttributes($values, $safeOnly);
+		foreach ($values as $name => $value) 
+		{
+			$name = $this->resolveModelAttribute($name);
+			if (is_array($name)) 
+			{
+				list($name, $attribute) = $name;
+				if (isset($this->models[$name]))
+					$this->models[$name]->{$attribute} = $value;
+			}
+		}
+	}
+
+	/**
+	 * Performs the validation.
+	 * @param array $attributes list of attributes that should be validated. Defaults to null,
+	 * @param boolean $clearErrors whether to call {@link clearErrors} before performing validation
+	 * @return boolean whether the validation is successful without any error.
+	 */
+	public function validate($attributes = null, $clearErrors = true) 
+	{
+		$valid = parent::validate($attributes, $clearErrors);
+		foreach ($this->models as $model)
+			$valid = $model->validate(null, $clearErrors) && $valid; // only valid if all models are valid
+		return $valid;
+	}
+
+	/**
+	 * Returns the text label for the specified attribute.
+	 * @param string $attribute the attribute name
+	 * @return string the attribute label
+	 */
+	public function getAttributeLabel($attribute) 
+	{
+		$attribute = $this->resolveModelAttribute($attribute);
+		if (is_array($attribute)) 
+		{
+			list($modelName, $attributeName) = $attribute;
+			if (!empty($attributeName) && isset($this->models[$modelName]))
+				return $this->models[$modelName]->getAttributeLabel($attributeName);
+		} 
+		return parent::getAttributeLabel($attribute);
+	}
+
+	/**
+	 * Returns a value indicating whether there is any validation error.
+	 * @param string $attribute attribute name. Use null to check all attributes.
+	 * @return boolean whether there is any error.
+	 */
+	public function hasErrors($attribute = null) 
+	{
+		$attribute = $this->resolveModelAttribute($attribute);
+		if (is_array($attribute)) 
+		{
+			list($modelName, $attributeName) = $attribute;
+			if (isset($this->models[$modelName]))
+				return $this->models[$modelName]->hasErrors($attributeName);
+		}
+		return parent::hasErrors($attribute);
+	}
+
+	/**
+	 * Returns the errors for all attribute or a single attribute.
+	 * @param string $attribute attribute name. Use null to retrieve errors for all attributes.
+	 * @return array errors for all attributes or the specified attribute. Empty array is returned if no error.
+	 */
+	public function getErrors($attribute = null) 
+	{
+		$errors = parent::getErrors($attribute);
+		foreach ($this->models as $model)
+			$errors = array_merge($errors, $model->getErrors($attribute));
+		return $errors;
+	}
+
+	/**
+	 * Returns the first error of the specified attribute.
+	 * @param string $attribute attribute name.
+	 * @return string the error message. Null is returned if no error.
+	 */
+	public function getError($attribute) 
+	{
+		$attribute = $this->resolveModelAttribute($attribute);
+		if (is_array($attribute)) 
+		{
+			list($modelName, $attribute) = $attribute;
+			if (!empty($attribute) && isset($this->models[$modelName]))
+				return $this->models[$modelName]->getError($attribute);
+		}
+		return parent::getError($attribute);
+	}
+
+	/**
+	 * Turns model.attribute into array($model, $attribute)
+	 * @param $attribute the attribute name.
+	 * @return string|array the attribute and model if applicable.
+	 */
+	public function resolveModelAttribute($attribute) 
+	{
+		return strpos($attribute, '.') > 0 ? explode('.', $attribute) : $attribute;
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-Yii-ModelGroupForm
-==================
+ModelGroupForm
+==============
+
+A form model that can handle other models in massive way.
+
+## Usage
+
+You can either use this model directly or extend it. Here's an example on its usage:
+
+**ExampleController.php**
+```php
+// Create some models.
+$user = new User;
+$profile = new Profile;
+$workAddress = new Address;
+$homeAddress = new Address;
+
+// Create the model group form instance.
+$model = new ModelGroupForm;
+
+// Assign the models to the form model.
+$model->models = array(
+	'user' => $user,
+	'profile' => $profile,
+	'homeAddress' => $homeAddress,
+	'workAddress' => $workAddress,
+);
+
+// Check whether the form is submitted.
+if (isset($_POST['ModelGroupFrom'])) // post key = model name
+{
+	// Magically assign all attributes to their models.
+	$model->attributes = $_POST['ModelGroupForm'];
+
+	// Validate all models at once.
+	if ($model->validate()) {
+		// Model is valid, do what you please.
+	}
+}
+
+$this->render('example-view', array('model' => $model));
+
+```
+
+**example-view.php**
+```php
+<?php $form = $this->widget('CActiveForm'); ?>
+
+<!-- user model fields -->
+<?php echo $form->textField($model, 'user.name'); ?>
+<?php echo $form->passwordField($model, 'user.password'); ?>
+
+<!-- profile model fields -->
+<?php echo $form->textField($model, 'profile.firstName'); ?>
+<?php echo $form->textField($model, 'profile.lastName'); ?>
+
+<!-- homeAddress model fields -->
+<?php echo $form->textField($model, 'homeAddress.street'); ?>
+<?php echo $form->textField($model, 'homeAddress.city'); ?>
+<?php echo $form->textField($model, 'homeAddress.postCode'); ?>
+<?php echo $form->textField($model, 'homeAddress.country'); ?>
+
+<!-- workAddress model fields -->
+<?php echo $form->textField($model, 'workAddress.street'); ?>
+<?php echo $form->textField($model, 'workAddress.city'); ?>
+<?php echo $form->textField($model, 'workAddress.postCode'); ?>
+<?php echo $form->textField($model, 'workAddress.country'); ?>
+
+<?php echo CHtml::submitButton('Submit'); ?>
+
+<?php $this->endWidget(); ?>
+```
+
+And that's all that is to it.
